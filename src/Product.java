abf48214153630cd8a7f1fb1df7cdeac7402eb10
Haidar N. <ub.haidarnadhifathaputra@gmail.com>
2026-03-11 20:46:22 +0700

    Inisialisasi Abstract Product

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,52 @@
 package src;
 
 public abstract class Product {
+    // Atribut
+    private String productId;
+    private String name;
+    private Double price;
+    private int stockQuantity;
+
+    // Constructor
+    public Product(String productId, String name, Double price, int stockQuantity){
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    // Method
+    public abstract Double calculateDiscount();
+    public void getProductInfo(){
+        System.out.println("Product ID     : " + productId);
+        System.out.println("Name           : " + name);
+        System.out.println("Price          : " + price);
+        System.out.println("Stock Quantity : " + stockQuantity);
+    }
+
+    public void updateStock(int x){
+        this.stockQuantity = x;
+    }
+
+    public void updateStock(int x, String info){
+        this.stockQuantity = x;
+        System.out.println(info);
+    }
+
+    // Accessor
+    public String getProductId(){
+        return this.productId;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Double getPrice(){
+        return this.price;
+    }
+
+    public int getStockQuantity(){
+        return this.stockQuantity;
+    }
 }
