fe860edbebb0ab8a7ac593ae9a34650aaf60b38a
BagasZakaria <bagaszakaria1004@gmail.com>
2026-03-11 22:08:34 +0700

    Update FoodProduct.java

--- src/FoodProduct.java
+++ src/FoodProduct.java
@@ -6,7 +6,6 @@ public class FoodProduct extends Product {
         super(productId, name, price, stockQuantity);
         this.expiryDate = expiryDate;
     }
-    
     @Override
     public double calculateDiscount() {
         if (getStockQuantity() > 10) {
@@ -14,11 +13,11 @@ public class FoodProduct extends Product {
         }
         return 0.0;
     }
-
     @Override
     public void getProductInfo() {
         super.getProductInfo();
         System.out.println("Tanggal kadaluwarsa: " + expiryDate);
     }
 }
+
 
