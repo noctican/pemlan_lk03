a1110f768b46d08fb347a5698109a83515faf4c6
BagasZakaria <bagaszakaria1004@gmail.com>
2026-03-11 21:06:31 +0700

    Add files via upload

---
+++ src/FoodProduct.java
@@ -0,0 +1,23 @@
+package src;
+public class FoodProduct extends Product {
+    private String expiryDate; 
+
+    public FoodProduct(String productId, String name, double price, int stockQuantity, String expiryDate) {
+        super(productId, name, price, stockQuantity);
+        this.expiryDate = expiryDate;
+    }
+    
+    @Override
+    public Double calculateDiscount() {
+        if (getStockQuantity() > 10) {
+            return 0.10; 
+        }
+        return 0.0;
+    }
+
+    @Override
+    public void getProductInfo() {
+        super.getProductInfo();
+        System.out.println("Expiry Date: " + expiryDate);
+    }
+}
